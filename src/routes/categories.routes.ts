fe61f9284f8eba9c52bd843581dc21b29fc6f069
igormarims <igormarquespe@gmail.com>
2023-07-28 19:49:11 -0300

    Model Categories e uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,27 @@
 import { Request, Response, Router } from 'express'
-import { v4 as uuidV4 } from 'uuid';
+
+import { Category } from '../model/Category';
  const categoriesRoutes = Router()
 
-   const categories = [];
+   const categories: Category[] = [];
 
  categoriesRoutes.post("/",(request: Request, response: Response)=> {
         const { name, description } = request.body
         
-        const category = {
-            name,
-            description,
-            id: uuidV4()
-        }
+        const category: Category = new Category()
+        //   category.name = name;
+        //   category.description = description;
+        //   category.created_at = new Date()
+         
+        Object.assign(category, {
+            name, 
+            description, 
+            creatted_at: new Date()
+            })
 
         categories.push(category)
 
-        return response.status(201).send();
+        return response.status(201).json({category});
    })
 
 
