7ae46bdb4bac4fda9b9e5d6d5d06cb36b7705c96
igormarims <igormarquespe@gmail.com>
2023-07-28 19:25:17 -0300

    Inserting typing for category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,19 @@
 import { Request, Response, Router } from 'express'
-
+import { v4 as uuidV4 } from 'uuid';
  const categoriesRoutes = Router()
 
    const categories = [];
 
-   categoriesRoutes.post("/categories",(request: Request, response: Response)=> {
+ categoriesRoutes.post("/",(request: Request, response: Response)=> {
         const { name, description } = request.body
-         
-        categories.push({
+        
+        const category = {
             name,
-            description
-        })
+            description,
+            id: uuidV4()
+        }
+
+        categories.push(category)
 
         return response.status(201).send();
    })
